a251d978f11bffcf5746131800348918e818180d
ManasaMamidipaka <manuramakrishna499@gmail.com>
2024-03-06 06:24:12 +0000

    solution

---
+++ src/components/Course/index.js
@@ -0,0 +1,22 @@
+import React, {Component} from 'react'
+import {Link} from 'react-router-dom'
+import './index.css'
+
+class Course extends Component {
+  render() {
+    const {details} = this.props
+    const {id, logoUrl, name} = details
+    return (
+      <Link to={`courses/${id}`}>
+        <li className="list-item">
+          <div>
+            <img src={logoUrl} alt={name} />
+          </div>
+          <p className="link-items">{name}</p>
+        </li>
+      </Link>
+    )
+  }
+}
+
+export default Course
